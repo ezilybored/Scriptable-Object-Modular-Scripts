8b465092b2ecdd74c3e6d3cfea25171fbb8010a7
ezilybored <33623970+ezilybored@users.noreply.github.com>
2018-05-18 12:09:22 +0100

    Create NPCItemTrackerScriptableObject.cs

---
+++ NPCItemTrackerScriptableObject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "NPCItemTracker", menuName = "Scriptable Objects", order = 1)]
+
+public class NPCItemTrackerScriptableObject : ScriptableObject {
+    
+    public Item itemOne;
+    
+    public Item itemTwo;
+    
+    public Item itemThree;
+
+}
