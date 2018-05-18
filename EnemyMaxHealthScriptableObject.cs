14389d08e9edeed65ac5ebf421efaa09584dfd96
ezilybored <33623970+ezilybored@users.noreply.github.com>
2018-05-18 11:51:01 +0100

    Update EnemyMaxHealthScriptableObject.cs

--- EnemyMaxHealthScriptableObject.cs
+++ EnemyMaxHealthScriptableObject.cs
@@ -1,6 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "EnemyMaxHealth", menuName = "Scriptable Objects", order = 1)]
+
+public class EnemyMaxHealthScriptableObject : ScriptableObject {
+    
+    public float EnemyMaxHealth;
+
+}
 
 
 
 
 
-EnemyMaxHealthScriptableObject
