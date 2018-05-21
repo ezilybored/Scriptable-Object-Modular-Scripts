fe3f03198b4fba650d708ceaee645e260d407f88
ezilybored <33623970+ezilybored@users.noreply.github.com>
2018-05-21 13:52:59 +0100

    Update PlayerPositionTrackerScriptableObject.cs

--- PlayerPositionTrackerScriptableObject.cs
+++ PlayerPositionTrackerScriptableObject.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 
+//This will not work as a SO is an asset and can not get any information from gameobjects
+
 [CreateAssetMenu(fileName = "PlayerPositionTracker", menuName = "Scriptable Objects", order = 1)]
 
 public class PlayerPositionTrackerScriptableObject : ScriptableObject {
